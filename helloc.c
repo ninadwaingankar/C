2a723dad991698b1f1b892d23149b38600cc912d
ninadwaingankar <ninadnw88@gmail.com>
2020-12-16 19:47:55 +0530

    This C program prints hello world

---
+++ helloc.c
@@ -0,0 +1,15 @@
+//this is my first C program
+// this prints only hello world
+#include<stdio.h>
+int main()
+{
+	int i;
+	while(i<995)
+	{
+		
+	printf("Hello, World!\n");
+	i++;
+	}
+	return 0;
+
+}
