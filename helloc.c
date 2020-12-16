284d021898457d5fc5d13c2a7793a68ffb31a4f7
ninadwaingankar <ninadnw88@gmail.com>
2020-12-16 20:10:56 +0530

    added square and cube funtions

--- helloc.c
+++ helloc.c
@@ -1,5 +1,40 @@
 //why C is best
+//square and cube functions implemented
+
+int square();
+int cube();
+
+#include<stdio.h>
+
 int main()
 {
+	int n,x,y;
+	
+	printf("Enter no \n");
+	scanf("%d",&n);
+	x=square(n);
+	y=cube(n);
+	
+	
+	printf("This is square %d\n",x);
+	
+	printf("This is cube %d\n",y);
+	
 	return 0;
 }
+
+//this function caqlculates square
+int square(int n)
+{
+	return n*n;
+}
+
+//this function caqlculates cube
+int cube(int n)
+{
+	return n*n*n;
+}
+
+
+
+
